891c547c7f5f1fcaf71fd3efc10bf9ba662fe650
dathoccode <datphungkl@gmail.com>
2025-10-27 10:08:30 +0700

    seperate layout Map view

--- Data/DBInit.sql
+++ Data/DBInit.sql
@@ -1,49 +1,5 @@
 use QuanLySanTheThao
 
-create table tPrice(
-	CourtID nvarchar(10),
-	SlotID nvarchar(10),
-	StartTime time,
-	EndTime time,
-	UnitPrice money,
-	primary key(CourtID, SlotID),
-	foreign key(CourtID) references ,
-	foreign key(SlotID) references
-)
-
-create table tBookingDetail(
-	DetailID nvarchar(10),
-	BookingID nvarchar(10),
-	CourtID nvarchar(10),
-	SlotID nvarchar(10),
-	StartTime time,
-	EndTime time,
-	primary key(DetailID),
-	foreign key(BookingID) references,
-	foreign key(CourtID) references,
-	foreign key(SlotID) references
-)
-
-create table tSlot(
-	SlotID nvarchar(10),
-	CourtID nvarchar(10),
-	SlotType nvarchar(100),
-	primary key(SlotID, CourtID),
-	foreign key(CourtID) references
-)
-
-create table tBooking(
-	BookingID nvarchar(10),
-	AccountID nvarchar(10),
-	BookingDate datetime,
-	Sale float null,
-	StatusID int,
-	Price money,
-	primary key(BookingID),
-	foreign key(AccountID) references,
-	foreign key(StatusID) references
-)
-
 create table tCourt(
 	CourtID nvarchar(10),
 	CourtName nvarchar(10),
@@ -56,12 +12,28 @@ create table tCourt(
 	primary key(CourtID)
 )
 
-create table tFavoriteCourt(
+create table tSlot(
+	SlotID nvarchar(10),
 	CourtID nvarchar(10),
-	AccountID nvarchar(10),
-	primary key(CourtID, AccountID),
-	foreign key(CourtID) references,
-	foreign key(AccountID) references
+	SlotType nvarchar(100),
+	primary key(SlotID, CourtID),
+	foreign key(CourtID) references tCourt(CourtID)
+)
+
+create table tPrice(
+	CourtID nvarchar(10),
+	SlotID nvarchar(10),
+	StartTime time,
+	EndTime time,
+	UnitPrice money,
+	primary key(CourtID, SlotID),
+	foreign key(CourtID) references tCourt(CourtID),
+)
+
+create table tRole(
+	RoleID int,
+	RoleName nvarchar(20),
+	primary key(RoleID)
 )
 
 create table tAccount(
@@ -74,7 +46,41 @@ create table tAccount(
 	foreign key(RoleID) references tRole(RoleID)
 )
 
-create table tRole(
-	RoleID int,
-	RoleName nvarchar(20)
+create table tStatus(
+	StatusID int not null,
+	StatusName nvarchar(30),
+	primary key(StatusID)
 )
+
+create table tBooking(
+	BookingID nvarchar(10),
+	AccountID nvarchar(10),
+	BookingDate datetime,
+	Sale float null,
+	StatusID int,
+	Price money,
+	primary key(BookingID),
+	foreign key(AccountID) references tAccount(AccountID),
+	foreign key(StatusID) references tStatus(StatusID)
+)
+
+create table tBookingDetail(
+	DetailID nvarchar(10),
+	BookingID nvarchar(10),
+	CourtID nvarchar(10),
+	SlotID nvarchar(10),
+	StartTime time,
+	EndTime time,
+	primary key(DetailID),
+	foreign key(BookingID) references tBooking(BookingID),
+	foreign key(CourtID) references tCourt(CourtID),
+)
+
+create table tFavoriteCourt(
+	CourtID nvarchar(10),
+	AccountID nvarchar(10),
+	primary key(CourtID, AccountID),
+	foreign key(CourtID) references tCourt(CourtID),
+	foreign key(AccountID) references tAccount(AccountID)
+)
+
